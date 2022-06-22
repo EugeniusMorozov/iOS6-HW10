c075979ab6b59261d4fae305f707d538d3a376bb
Eugenius Morozov <emorozov777@gmail.com>
2022-06-22 15:55:02 +0300

    Initial Commit

---
+++ iOS6-HW10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS6-HW10
+//
+//  Created by Evgeny Morozov on 22.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
